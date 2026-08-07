adcbc33337f3d3134951afbb23ab55d5a74c19d5
sumitparmar007 <parmatsumit06830@gmail.com>
2026-08-07 10:44:03 +0530

    modify the about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,10 +5,19 @@ function About() {
       <h2>About Me</h2>
 
       <p>
-       Computer Engineering student passionate about Artificial Intelligence, Machine Learning, and Data Science. Skilled
-    in Python, Machine Learning, Deep Learning, Data Analysis, SQL, and AI model development. Experienced in
-    building AI-powered applications using Scikit-learn, TensorFlow, Pandas, NumPy, FastAPI, and OpenCV. Passionate
-    about solving real-world problems using AI and continuously learning emerging technologies.
+       
+
+            Computer Engineering student and aspiring Python Developer with hands-on knowledge
+          of Python programming, Object-Oriented Programming (OOP), Data Structures, SQL,
+          REST API development, Machine Learning, Deep Learning, and Data Analysis. 
+          Experienced in developing AI/ML models and Python-based backend applications 
+          using FastAPI, Scikit-learn, TensorFlow, Pandas, NumPy, and OpenCV. Skilled in 
+          data  preprocessing, exploratory data analysis (EDA), feature engineering, 
+          model training, evaluation,and deployment. Familiar with MySQL, Git, GitHub,
+          Jupyter Notebook, and VS Code. Interested  in building scalable Python applications, 
+          AI-powered solutions, automation systems, and data-driven applications.
+
+
       </p>
 
     </section>
